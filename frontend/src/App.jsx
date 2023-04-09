713684c4aaae0affe341f111854c09298885cc5c
r11921a37 <r11921a37@ntu.edu.tw>
2023-04-09 10:22:50 +0800

    backend and frontend connection without glob

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import Welcome from './Welcome'
 import ColorText from './ColorText'
+import services from "./services";
 function App() {
+  services.user.getAll().then((data) => {
+    console.log(data);
+    }); 
   return (
     <div>
       <Welcome name="Yiting" />
