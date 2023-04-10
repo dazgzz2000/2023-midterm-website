5d562ffadb19853a9df98d9e7c485f0f5f13badc
r11921a37 <r11921a37@ntu.edu.tw>
2023-04-10 19:52:04 +0800

    user api addition from tutorial

--- backend/src/routes/api/v1/users/index.js
+++ backend/src/routes/api/v1/users/index.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { getAllUsers } from "./handler";
 const router = Router();
-router.get(`/`, getAllUsers);
+router.get('/', getAllUsers);
+router.post('/', createOneUser);
+router.get('/:id', getOneUser);
 export default router;
