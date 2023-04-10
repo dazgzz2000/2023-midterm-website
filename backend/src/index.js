93b257010dcaa5528b1f4ba0af68e37f6d2ecad4
r11921a37 <r11921a37@ntu.edu.tw>
2023-04-10 19:27:14 +0800

    prisma db

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 import birds from "./birds";
 import rootRouter from "./routes";
+import { prisma } from "./adapters";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const frontendDir = path.join(__dirname, "../../frontend/dist");
 const port = process.env.PORT || 8000;
@@ -20,4 +21,7 @@ app.get("*", (req, res) => { // Keep as the last route
 });
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
+});
+process.on("exit", async () => {
+    await prisma.$disconnect();
 });
